12067676ae9ec4d75ef02875eb2745350231a7b7
velcro777 <sean.lucas9@gmail.com>
2021-12-03 22:47:30 +0000

    status

--- pages/views.py
+++ pages/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def index(request):
-    return render(request, 'pages/index.html')
+def home(request):
+    return render(request, 'pages/home.html')
